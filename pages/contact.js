3d19e2d9f5031a748fb82657447db2686f92a8a0
John.Stephenson.STL <John.Stephenson.STL@gmail.com>
2020-04-14 21:33:53 -0500

    First Commit Next.js API

---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const Contact = () => (
+  <Layout>
+    <div>
+      <h1>This is the page to Contact Me!</h1>
+    </div>
+  </Layout>
+)
+
+export default Contact;
---
+++ pages/info.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const Info = () => (
+  <Layout>
+    <div>
+      <h1>This is the page to display information!</h1>
+    </div>
+  </Layout>
+)
+
+export default Info;
